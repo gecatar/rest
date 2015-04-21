b46d04ca7a566a4db0d9e83c02044754322c45d7
estefanov <estefanov@10.131.2.242>
2015-04-21 13:00:14 +0300

    rtyrty

--- adressbook/src/com/sirma/train/adressbook/AdressApp.java
+++
@@ -1,9 +0,0 @@
-package com.sirma.train.adressbook;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/adresses")
-public class AdressApp extends Application {
-
-}
